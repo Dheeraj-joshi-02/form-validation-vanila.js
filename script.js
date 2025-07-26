972376d4e3d01ac2bf1745273cb3fd3177384d23
dheeraj-Joshi0208 <dheerajjoshi0208@gmail.com>
2025-07-27 02:52:06 +0530

    Complete for use

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const form = document.querySelector("#form");
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirmPassword");
+const message = document.querySelector("#message");
 
 const nameError = document.querySelector(".name-error");
 const emailError = document.querySelector(".email-error");
 const passwordError = document.querySelector(".password-error");
+const confirmPasswordError = document.querySelector(".confirm-password-error");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -15,11 +18,14 @@ form.addEventListener("submit", (event) => {
   const passwordRegex =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?#&^])[A-Za-z\d@$!%*?#&^]{8,}$/;
 
+  let isValid = true;
+
   // Validate Name
   const nameValue = nameInput.value.trim();
   if (!usernameRegex.test(nameValue)) {
     nameError.style.display = "block";
     nameInput.style.border = "2px solid red";
+    isValid = false;
   } else {
     nameError.style.display = "none";
     nameInput.style.border = "2px solid green";
@@ -30,6 +36,7 @@ form.addEventListener("submit", (event) => {
   if (!emailRegex.test(emailValue)) {
     emailError.style.display = "block";
     emailInput.style.border = "2px solid red";
+    isValid = false;
   } else {
     emailError.style.display = "none";
     emailInput.style.border = "2px solid green";
@@ -40,8 +47,30 @@ form.addEventListener("submit", (event) => {
   if (!passwordRegex.test(passwordValue)) {
     passwordError.style.display = "block";
     passwordInput.style.border = "2px solid red";
+    isValid = false;
   } else {
     passwordError.style.display = "none";
     passwordInput.style.border = "2px solid green";
   }
+
+  // Validate Confirm Password
+  const confirmPasswordValue = confirmPassword.value.trim();
+  if (passwordValue !== confirmPasswordValue) {
+    confirmPasswordError.style.display = "block";
+    confirmPassword.style.border = "2px solid red";
+    isValid = false;
+  } else {
+    confirmPasswordError.style.display = "none";
+    confirmPassword.style.border = "2px solid green";
+  }
+
+  // Final message
+  if (!isValid) {
+    message.innerHTML = "❌Invalid credentials. Please check the highlighted fields.";
+    message.style.color = "red"
+  } else {
+    message.innerHTML = "✅Form submitted successfully";
+    message.style.color = "green"
+  }
+  console.log(nameValue, emailValue, passwordValue, confirmPasswordValue);
 });
